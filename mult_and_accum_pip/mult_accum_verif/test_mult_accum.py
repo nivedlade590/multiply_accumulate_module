d9a240f78436f727cab51cbe1dc43f2eea53cd99
nivedlade590 <53433958+nivedlade590@users.noreply.github.com>
2024-11-26 14:47:04 +0530

    Add files via upload

---
+++ mult_and_accum_pip/mult_accum_verif/test_mult_accum.py
@@ -0,0 +1,164 @@
+# This file is public domain, it can be freely copied without restrictions.
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
+from model_mult_accum import *
+
+@cocotb.test()
+
+async def test_mult_accum(dut):
+
+	clock = Clock(dut.CLK, 10, units="us")  # Create a 10us period clock on port clk
+	# Start the clock. Start it low to avoid issues on the first RisingEdge
+	cocotb.start_soon(clock.start(start_high=False))
+
+	## test using model
+	dut.RST_N.value = 0
+	await RisingEdge(dut.CLK)
+	dut.RST_N.value = 1
+
+	a_bin=os.path.join(os.getcwd(),"mult_accum_verif/int8 MAC/A_binary.txt")
+	with open(a_bin, "r") as file:
+		a_list_bin = [int("00000000" + line.strip(), 2) for line in file]
+	
+	b_bin=os.path.join(os.getcwd(),"mult_accum_verif/int8 MAC/B_binary.txt")
+	with open(b_bin, "r") as file:
+		b_list_bin = [int("00000000" + line.strip(), 2) for line in file]
+
+	c_bin=os.path.join(os.getcwd(),"mult_accum_verif/int8 MAC/C_binary.txt")
+	with open(c_bin, "r") as file:
+		c_list_bin = [int("00000000" + line.strip(), 2) for line in file]
+	
+	mac_dec=os.path.join(os.getcwd(),"mult_accum_verif/int8 MAC/MAC_decimal.txt")
+	with open(mac_dec, "r") as file:
+		mac_list_dec = [line.strip() for line in file]
+	
+	#for int test cases
+	for i in range(0, 100): #1000
+			
+		dut.EN_get_A.value = 1
+		dut.EN_get_B.value = 1
+		dut.get_A_a.value = a_list_bin[i]
+		dut.get_B_b.value = b_list_bin[i]
+		
+		await RisingEdge(dut.CLK)
+		
+		dut.EN_get_C.value = 1
+		dut.get_C_c.value = c_list_bin[i]
+		
+		await RisingEdge(dut.CLK)
+		await RisingEdge(dut.CLK)
+		await RisingEdge(dut.CLK)
+		
+		
+		dut.EN_get_select.value = 1
+		dut.get_select_s.value = 0
+		
+		#dut._log.info(f'output {int(dut.result.value)}')
+		
+		result=int(dut.result.value)
+		
+		value=int(mac_list_dec[i])
+		if value < 0 :
+			value= value+4294967296;
+		
+		if result != value :
+			print(i,"fail",result,value)
+		else :
+			print(i,"succ",result,value)
+			
+		assert value == result , f'Mismatch,wrong assert,***{i}***'
+
+#********************************* fp32 functions ********************************************************************************
+#*********************************************************************************************************************************
+
+	a_bin=os.path.join(os.getcwd(),"mult_accum_verif/bf16 MAC/A_binary.txt")
+	with open(a_bin, "r") as file:
+		a_list_bin = [int("00000000" + line.strip(), 2) for line in file]
+	
+	b_bin=os.path.join(os.getcwd(),"mult_accum_verif/bf16 MAC/B_binary.txt")
+	with open(b_bin, "r") as file:
+		b_list_bin = [int("00000000" + line.strip(), 2) for line in file]
+
+	c_bin=os.path.join(os.getcwd(),"mult_accum_verif/bf16 MAC/C_binary.txt")
+	with open(c_bin, "r") as file:
+		c_list_bin = [int("00000000" + line.strip(), 2) for line in file]
+	
+	mac_bin=os.path.join(os.getcwd(),"mult_accum_verif/bf16 MAC/MAC_binary.txt")
+	with open(mac_bin, "r") as file:
+		mac_list_bin = [line.strip() for line in file]
+	
+	mac_dec=os.path.join(os.getcwd(),"mult_accum_verif/bf16 MAC/MAC_decimal.txt")
+	with open(mac_dec, "r") as file:
+		mac_list_dec = [line.strip() for line in file]
+	
+	#flush cycle
+	dut.EN_get_A.value = 1
+	dut.EN_get_B.value = 1
+	dut.get_A_a.value = a_list_bin[0]
+	dut.get_B_b.value = b_list_bin[0]
+	
+	await RisingEdge(dut.CLK)
+	
+	dut.EN_get_C.value = 1
+	dut.get_C_c.value = c_list_bin[0]
+	
+	await RisingEdge(dut.CLK)
+	await RisingEdge(dut.CLK)
+	await RisingEdge(dut.CLK)
+	
+	dut.EN_get_select.value = 1
+	dut.get_select_s.value = 1
+	
+	#dut._log.info(f'output {int(dut.result.value)}')
+	
+	result=int(dut.result.value)
+	#for float test cases
+	for i in range(0, 100): #1000
+	
+		dut.EN_get_A.value = 1
+		dut.EN_get_B.value = 1
+		dut.get_A_a.value = a_list_bin[i]
+		dut.get_B_b.value = b_list_bin[i]
+		
+		await RisingEdge(dut.CLK)
+		
+		dut.EN_get_C.value = 1
+		dut.get_C_c.value = c_list_bin[i]
+		
+		await RisingEdge(dut.CLK)
+		await RisingEdge(dut.CLK)
+		await RisingEdge(dut.CLK)
+		
+		dut.EN_get_select.value = 1
+		dut.get_select_s.value = 1
+		
+		#dut._log.info(f'output {(dut.result.value)}')
+		
+		result=(dut.result.value)
+		result=result.binstr
+		
+		value=(mac_list_bin[i])
+		#if result[0:29] == value[0:29] :
+		#	print("success")
+		#else :
+		#	print("failure")
+		
+		if result[0:29] != value[0:29] :
+			print(i,"fail",result,value)
+			#print(i,"fail")
+			#print(result)
+			#print(value)
+		else :
+			print(i,"succ",result,value)
+
+		#print(i,result,value);
+		#assert result[0:29] == value[0:29] , f'Mismatch,wrong assert,***{i}***'
+
+	coverage_db.export_to_yaml(filename="coverage_counter.yml")
